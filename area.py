013742bb5985dd857b25207f27ad1c792b6dd5ef
muki0901 <109414784+muki0901@users.noreply.github.com>
2022-07-16 15:36:00 +0530

    updated area.py
    This will show the output of area of circle.

--- area.py
+++ area.py
@@ -1,5 +1,5 @@
 rad = float(input("Enter the radius of circle: "))
-x = 3.14159
-y = pow(rad,2)
-AOC = x*y
-print(AOC)
+x = 3.14159    #pi value
+y = pow(rad,2) 
+AOC = x*y      #area of circle
+print(AOC)     # printing area of circle
