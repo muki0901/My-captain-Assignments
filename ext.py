8ea443f9003df505080f6925568207807ccd09c2
muki0901 <109414784+muki0901@users.noreply.github.com>
2022-07-16 15:44:48 +0530

    updated ext.py
    This will show the extension name of file.

--- ext.py
+++ ext.py
@@ -1,8 +1,8 @@
 file_name = input("Enter the file name: ")
 f = file_name.split(".")
-print(f)
-g = f[1]
-print(g)
+print(f)    # printing as list
+g = f[1]    
+print(g)    #printing extension name
 if g=="py":
     print("The extension of the file is python")
 
